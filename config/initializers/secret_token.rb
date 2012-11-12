424a4b3d2fc8678f32dacc07d6c6cc5950af40d4
U-JakovPavlek-PC\Jakov Pavlek <Jakov Pavlek@JakovPavlek-PC.(none)>
2012-11-12 10:17:51 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projektx::Application.config.secret_token = '74fa2dc72dbbdffd44d81c6519544f725367530574eb5b014b8544734628204bc49ba1a924a39998b5f32716b6e0cd88e17980cba0871668bc49b2fef7578ff2'
